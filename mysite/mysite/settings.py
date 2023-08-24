0a6b7a2acf702a64b362ba5b6dd12625305b4d88
kartikeya <kartikeymahajan123@gmail.com>
2023-08-24 17:53:20 +0530

    new zealand Time added

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'machines.apps.MachinesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -127,3 +126,11 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'machines', 'static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Settings for email notification. 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@gmail.com'  # Your Gmail address
+EMAIL_HOST_PASSWORD = 'your_password'     # Your Gmail password or an app password
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,6 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("machines.urls")),
-    path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
